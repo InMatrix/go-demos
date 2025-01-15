c29726a2da93ccb2c96fe3da1ab84dd78ca30f42
Tao Dong <tomdong@gmail.com>
2025-01-15 05:51:49 +0000

    Add another quiz

--- capital_quiz.go
+++ capital_quiz.go
@@ -14,19 +14,35 @@ func main() {
         fmt.Println("\033[32mWelcome to the Go Quiz Game!\033[0m")
         time.Sleep(2 * time.Second)
 
-        // Ask a question
+        // Question 1
         fmt.Println("\nWhat is the capital of France?")
         fmt.Println("a) Berlin")
         fmt.Println("b) Paris")
         fmt.Println("c) Rome")
 
-        // Get user input
-        var answer string
+        var answer1 string
         fmt.Print("Your answer: ")
-        fmt.Scanln(&answer)
+        fmt.Scanln(&answer1)
 
-        // Check answer and provide feedback
-        if answer == "b" || answer == "B" {
+        if answer1 == "b" || answer1 == "B" {
+                fmt.Println("\033[32mCorrect!\033[0m")
+        } else {
+                fmt.Println("\033[31mIncorrect.\033[0m")
+        }
+
+        time.Sleep(1 * time.Second)
+
+        // Question 2
+        fmt.Println("\nWhat is the capital of Japan?")
+        fmt.Println("a) Beijing")
+        fmt.Println("b) Seoul")
+        fmt.Println("c) Tokyo")
+
+        var answer2 string
+        fmt.Print("Your answer: ")
+        fmt.Scanln(&answer2)
+
+        if answer2 == "c" || answer2 == "C" {
                 fmt.Println("\033[32mCorrect!\033[0m")
         } else {
                 fmt.Println("\033[31mIncorrect.\033[0m")
